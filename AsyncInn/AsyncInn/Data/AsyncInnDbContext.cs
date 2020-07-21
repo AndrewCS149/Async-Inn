7feb1c842b697803da1d0c336394d6726d12a004
Andrew Smith <andrewcs149@gmail.com>
2020-07-21 11:10:11 -0700

    Add migrations.

--- AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AsyncInn.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,9 @@ namespace AsyncInn.Data
 {
     public class AsyncInnDbContext : DbContext
     {
+        public DbSet<Hotel> Hotels { get; set; }
+
+
         public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
         {
 
