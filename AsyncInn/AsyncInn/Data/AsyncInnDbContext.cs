45a4d9f664a7f08885298f980a02582585479201
Andrew Smith <andrewcs149@gmail.com>
2020-07-21 10:28:26 -0700

    Add Data folder and add AsyncInnDbContext class to it.

---
+++ AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+    }
+}
