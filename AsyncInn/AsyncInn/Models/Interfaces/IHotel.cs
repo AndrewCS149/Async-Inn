36c79b3123bea1299bf2b79dd8586ffc01e03cc4
Andrew Smith <andrewcs149@gmail.com>
2020-07-22 10:42:13 -0700

    Add Hotel interface and methods.

---
+++ AsyncInn/AsyncInn/Models/Interfaces/IHotel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces
+{
+    interface IHotel
+    {
+        // contain methods and properties that are required for the classes to implement 
+
+        // create
+        Hotel Create(Hotel hotel);
+
+        // read
+        // get all
+        List<Hotel> GetAllHotels();
+
+        // get individually (by id)
+        Hotel GetHotel(int id);
+
+        // update
+        Hotel Update(int id, Hotel hotel);
+
+        // delete
+        void Delete(int id);
+    }
+}
