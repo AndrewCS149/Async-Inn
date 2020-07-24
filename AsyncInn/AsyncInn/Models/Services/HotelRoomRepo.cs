e4e72c52ef2bc0e80b1196f54488fdb823920c39
Andrew Smith <andrewcs149@gmail.com>
2020-07-23 23:26:13 -0700

    Implement interface methods to HotelRoomRepo.

--- AsyncInn/AsyncInn/Models/Services/HotelRoomRepo.cs
+++ AsyncInn/AsyncInn/Models/Services/HotelRoomRepo.cs
@@ -8,5 +8,39 @@ namespace AsyncInn.Models.Services
 {
     public class HotelRoomRepo : IHotelRoom
     {
+        public Task AddRoomToHotel(int roomId, int hotelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoom> Create(HotelRoom hotelRoom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<HotelRoom>> GetAllHotelRooms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoom> GetHotelRoom(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveRoomFromHotel(int roomId, int hotelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoom> Update(HotelRoom hotelRoom)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
