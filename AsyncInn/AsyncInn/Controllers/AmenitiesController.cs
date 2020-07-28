2c96856f22772128d07c7b14078ee3fbe92e4500
Andrew Smith <andrewcs149@gmail.com>
2020-07-27 18:54:38 -0700

    Update GetAmenity().

--- AsyncInn/AsyncInn/Controllers/AmenitiesController.cs
+++ AsyncInn/AsyncInn/Controllers/AmenitiesController.cs
@@ -19,37 +19,27 @@ namespace AsyncInn.Controllers
         private readonly IAmenities _amenity;
         private readonly AsyncInnDbContext _context;
 
-        public AmenitiesController(IAmenities amenity, AsyncInnDbContext context)
+        public AmenitiesController(IAmenities amenity)
         {
-            _context = context;
             _amenity = amenity;
         }
-
-        //// GET: api/Amenities
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<Amenities>>> GetAllAmenities()
-        //{
-        //    return await _amenity.GetAllAmenities();
-        //}
-
-        // GET: api/Amenities/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Amenities>> GetAmenity(int id)
-        //{
-        //    return await _amenity.GetAmenity(id);
-        //}
 
         // GET: api/Amenities/4
         [HttpGet]
         [Route("{id}")]
-        public async Task<AmenityDTO> GetAmenity(int id)
+        public async Task<ActionResult<AmenityDTO>> GetAmenity(int id)
         {
-            return await _amenity.GetAmenity(id);
+            var amenities = await _amenity.GetAmenity(id);
+
+            if (amenities == null)
+                return NotFound();
+
+            return amenities;
         }
 
         // GET: api/Amenities
         [HttpGet]
-        public IQueryable<AmenityDTO> GetAmenities()
+        public async Task<ActionResult<AmenityDTO>> GetAmenities()
         {
             var amenities = from a in _context.Amenities
                             select new AmenityDTO()
@@ -73,16 +63,6 @@ namespace AsyncInn.Controllers
             var updatedAmenity = await _amenity.Update(amenities);
             return Ok(updatedAmenity);
         }
-
-        // POST: api/Amenities
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        //[HttpPost]
-        //public async Task<ActionResult<Amenities>> PostAmenities(Amenities amenities)
-        //{
-        //    await _amenity.Create(amenities);
-        //    return CreatedAtAction("GetAmenities", new { id = amenities.Id }, amenities);
-        //}
 
         // TODO: not working
         // POST: api/Amenities
