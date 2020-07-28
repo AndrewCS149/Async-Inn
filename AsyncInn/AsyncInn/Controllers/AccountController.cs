0f576c0572bc2f9d57e4893c54fa25424e28fa72
Andrew Smith <andrewcs149@gmail.com>
2020-07-28 11:00:53 -0700

    Add RegisterDTO class.

---
+++ AsyncInn/AsyncInn/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsyncInn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        // POST: api/Account/register
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register()
+    }
+}
