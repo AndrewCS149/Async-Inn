66f6a3cb5486c5d8ed75fabbc1a2ad374ce8d1f7
Andrew Smith <andrewcs149@gmail.com>
2020-07-27 20:00:44 -0700

    Update RoomAmenities props and Hotel props.

--- AsyncInn/AsyncInn/Models/Hotel.cs
+++ AsyncInn/AsyncInn/Models/Hotel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,26 @@ namespace AsyncInn.Models
     public class Hotel
     {
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Hotel Name:")]
         public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Street Address:")]
         public string StreetAddress{ get; set; }
+
+        [Required]
         public string City { get; set; }
+
+        [Required]
         public string State { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number:")]
         public string Phone { get; set; }
 
         // Navigation Properties coming soon
-        public List<HotelRoom> HotelRoom { get; set; }
+        public ICollection<HotelRoom> HotelRoom { get; set; }
     }
 }
--- AsyncInn/AsyncInn/Models/RoomAmenities.cs
+++ AsyncInn/AsyncInn/Models/RoomAmenities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,12 @@ namespace AsyncInn.Models
 {
     public class RoomAmenities
     {
+        [Required]
+        [Display(Name = "Amenity Name:")]
         public int AmenitiesId { get; set; }
+
+        [Required]
+        [Display(Name = "Room Type:")]
         public int RoomId { get; set; }
 
         // navigation properties
