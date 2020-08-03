feac9d4236ab2aaa317ce76318cc0aa71a02c5f5
Andrew Smith <andrewcs149@gmail.com>
2020-08-02 20:52:10 -0700

    attempt route testing.

--- AsyncInn/AsyncInnTests/HotelTests.cs
+++ AsyncInn/AsyncInnTests/HotelTests.cs
@@ -11,13 +11,18 @@ namespace AsyncInnTests
 {
     public class HotelTests : DBTestBase
     {
-        private IHotel BuildRepository()
+        private IHotel BuildHotelRepo()
         {
             return new HotelRepo(_db, _hotelRoom);
         }
 
+        private IHotelRoom BuildHotelRoomRepo()
+        {
+            return new HotelRoomRepo(_db, _room);
+        }
+
         [Fact]
-        public async Task CanCreateAndReturnDTO()
+        public async Task CanCreateAndReturnHotelDTO()
         {
             // arrange
             var hotel = new Hotel
@@ -29,7 +34,7 @@ namespace AsyncInnTests
                 Phone = "8675309"
             };
 
-            var repo = BuildRepository();
+            var repo = BuildHotelRepo();
 
             HotelDTO hotelDTO = new HotelDTO()
             {
@@ -48,6 +53,61 @@ namespace AsyncInnTests
             Assert.NotNull(savedHotel);
             Assert.Equal(savedHotel.Id, hotel.Id);
             Assert.Equal(hotel.Name, savedHotel.Name);
+        }
+
+        // TODO: not working
+        [Fact]
+        public async Task CanCreateAndReturnHotelRoomDTO()
+        {
+            // arrange
+            var hotel = new Hotel
+            {
+                Id = 1,
+                Name = "Mariott",
+                City = "Seattle",
+                StreetAddress = "Main Street",
+                State = "WA",
+                Phone = "8675309"
+            };
+
+            HotelDTO hotelDTO = new HotelDTO()
+            {
+                Id = hotel.Id,
+                Name = hotel.Name,
+                StreetAddress = hotel.StreetAddress,
+                City = hotel.City,
+                State = hotel.State,
+                Phone = hotel.Phone
+            };
+
+            var hotelRoom = new HotelRoom
+            {
+                RoomId = 1,
+                RoomNumber = 1,
+                Rate = 100.00M,
+                PetFriendly = true
+            };
+
+            HotelRoomDTO hotelRoomDTO = new HotelRoomDTO()
+            {
+                HotelId = hotelRoom.HotelId,
+                RoomId = hotelRoom.RoomId,
+                RoomNumber = hotelRoom.RoomNumber,
+                Rate = hotelRoom.Rate,
+                PetFriendly = hotelRoom.PetFriendly
+            };
+
+            var hotelRepo = BuildHotelRepo();
+            var hotelRoomRepo = BuildHotelRoomRepo();
+            // act
+            var savedHotel = await hotelRepo.Create(hotelDTO);
+            var savedHotelRoom = await hotelRoomRepo.Create(hotelRoomDTO, savedHotel.Id);
+
+            // assert
+            Assert.NotNull(savedHotelRoom);
+            Assert.Equal(savedHotelRoom.HotelId, hotelRoom.HotelId);
+            Assert.Equal(hotelRoom.Rate, savedHotelRoom.Rate);
+            Assert.True(savedHotelRoom.PetFriendly);
         }
     }
 }
---
+++ AsyncInn/AsyncInnTests/Routes/HotelRouteTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsyncInnTests.Routes
+{
+    class HotelRouteTests
+    {
+    }
+}
